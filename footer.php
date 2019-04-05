20aaf8574462c1677057f5c48a7cf26eeb812f80
xcsoft <42080379+xc-soft@users.noreply.github.com>
2019-04-05 17:41:34 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
     <div class="mdui-divider-inset"></div>
         <!-- 版权 -->
     <center><p><a href='http://xsot.tk'>xsot.tk短网址</a></p><center>
+        <!-- 至所有用户:这个开源放在这里分享是为了让更多用户开心,希望你不要移除版权,如果移除版权,你的名字将被挂在我的博客上!在这基础上你可以自由修改源码!如果有任何疑问,可以联系我! -->->
     <center><p> &copy;<?php echo(date("Y")); ?> <a href='http://blog.xsot.cn'>XCSOFT</a> All Rights Reserved.</p><center>
 </footer>
