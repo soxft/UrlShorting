e70002e32f506984add59c1ec83966b4c5a05b06
xcsoft <42080379+xc-soft@users.noreply.github.com>
2019-05-01 19:20:46 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <div class="mdui-divider-inset"></div>
-        <!-- 至所有用户:这个开源放在这里分享是为了让更多用户开心,希望你不要移除版权,如果移除版权,你的名字将被挂在我的博客上!在这基础上你可以自由修改源码!如果有任何疑问,可以联系我! -->
-    <center><p> &copy;<?php echo(date("Y")); ?> <a href='http://xsot.cn'>XCSOFT</a> All Rights Reserved.</p><center>
-</footer>
