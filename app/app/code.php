c05786a8e664df5a3c08af6017232a45d9312622
xcsoft <email@xcsoft.top>
2020-08-13 17:32:39 +0800

    Delete code.php

--- app/app/code.php
+++
@@ -1,9 +0,0 @@
-<?php
-function parseurl($url="")
-  {
-  $url = urlencode($url);
-  $a = array("%3A", "%2F", "%40" , "%3F","%3D");
-  $b = array(":", "/", "@" ,"?","=");
-  $url = str_replace($a, $b, $url);
-  return $url;
-  }
