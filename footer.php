6fddbd748a7ce864d48a881061b2b214bc2a80cc
soxft <email@xcsoft.top>
2021-06-19 14:18:59 +0800

    注释myssl,需要的自己加上

--- footer.php
+++ footer.php
@@ -29,9 +29,7 @@
         <p id="hitokoto">:D 获取中...</p>
         <div class="footer-copyright">Copyright © 2019-
             <?php echo date( 'Y') ?> <a class="mdui-text-color-grey-800" href="http://xsot.cn">XCSOFT</a> All rights reserved.⚡ 
-            
             <p>Secondary Developed By <?php echo date( 'Y') ?>-<a class="mdui-text-color-grey-800" href=""> k6o</a>.😉</p>
-            <p><a class="mdui-text-color-grey-800" href="">This website is protected by Cloudflare.</a></p>
         </div>
        
         
@@ -52,15 +50,13 @@
                  setInterval("ShowRunTime('RunTime')", 1000);
                </script>      
     </center>
-    
+<!-- 
+    需要的话自己删除注释
 <center>
 <div title="MySSL 安全签章" id="myssl_seal" onclick="window.open('https://myssl.com/seal/detail?domain=<?php echo $host ?>','MySSL安全签章','height=800,width=470,top=0,right=0,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no')" style="text-align: center"><img src="https://sealres.myssl.com/seal/img/1x/seal.svg?domain=s.k6o.top" alt="MySSL 安全签章" style="width: 100px; height: auto; cursor: pointer"></div>
-
-
 </center>
-
 <div id="cc-myssl-id" style="position: fixed;right: 0;bottom: 0;width: 65px;height: 65px;z-index: 99;">
     <a href="https://myssl.com/<?php echo $host ?>?from=mysslid"><img src="https://static.myssl.com/res/images/myssl-id.png" alt="" style="width:100%;height:100%"></a>
 </div>
-
+ -->
 </footer>
