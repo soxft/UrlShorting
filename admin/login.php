7cc307ad33c129a402cb01c9d42c626c8e528f6b
xueJL <1392269217@qq.com>
2022-03-01 20:02:57 +0800

    mdui0.4.3 cdn change to cdn.bootcdn .net

--- admin/login.php
+++ admin/login.php
@@ -2,19 +2,17 @@
 require_once('../config.php');
 //包括上一个文件夹的config.php
 session_start();
-define('CLIENT_ID','8us3lhiuyiOlyT3KitpWvtIwGindm5');
-if(isset($_POST['passwd']))
-{
-  if($_POST['passwd'] == $passwd)
-  {
+define('CLIENT_ID', '8us3lhiuyiOlyT3KitpWvtIwGindm5');
+if (isset($_POST['passwd'])) {
+  if ($_POST['passwd'] == $passwd) {
     $_SESSION['password'] = $passwd;
     exit('200');
-  }else{
+  } else {
     exit('1001');
   }
 } else {
   //判断是否已经登录
-  if($_SESSION['password'] == $passwd){
+  if ($_SESSION['password'] == $passwd) {
     header("Refresh:0;url=\"./index.php\"");
     require_once '../footer.php';
     exit();
@@ -22,86 +20,85 @@ if(isset($_POST['passwd']))
 }
 ?>
 <link rel="icon" type="image/x-icon" href="https://cdn.jsdelivr.net/gh/soxft/cdn@1.9/urlshorting/favicon.ico" />
+
 <body background="https://cdn.jsdelivr.net/gh/soxft/cdn@1.9/urlshorting/background.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title><?php echo $title; ?></title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/soxft/cdn@master/mdui/css/mdui.min.css">
-    <script src="https://cdn.jsdelivr.net/gh/soxft/cdn@master/mdui/js/mdui.min.js"></script>
-      <style>
-        a {
-          text-decoration:none
-        }
-        a:hover {
-          text-decoration:none
-        }
-      </style>
-    <div class="mdui-container">
-      <div class="mdui-typo">
-        <h2 class="doc-chapter-title doc-chapter-title-first">登录后台</h2>
-              <!-- 浮动标签 -->
-              <div class="mdui-textfield mdui-textfield-floating-label">
-                  <label class="mdui-textfield-label">密码</label>
-                  <input id="password" type="password" class="mdui-textfield-input" />
-              <center>
-                  
-                  <button class="mdui-btn mdui-btn-raised mdui-ripple" id="btn" onclick="login()">登陆</button>
-                  
-                  
-                  <?php if(!empty(mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM config where type='xoauth'"))['content'])){ ?>
-                  <div style='height:10px'></div>
-                  <button class="mdui-btn mdui-btn-raised mdui-ripple" onclick="window.location.href='<?php echo "http://9420.ltd/v1/oauth.php?response_type=code&client_id=". CLIENT_ID ."&redirect_uri=".$url."app/oauth.php" ?>'">第三方登录</button>
-                  <?php } ?>
-              </center>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <title><?php echo $title; ?></title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/mdui/0.4.3/css/mdui.min.css">
+  <script src="https://cdn.bootcdn.net/ajax/libs/mdui/0.4.3/js/mdui.min.js"></script>
+  <style>
+    a {
+      text-decoration: none
+    }
+
+    a:hover {
+      text-decoration: none
+    }
+  </style>
+  <div class="mdui-container">
+    <div class="mdui-typo">
+      <h2 class="doc-chapter-title doc-chapter-title-first">登录后台</h2>
+      <!-- 浮动标签 -->
+      <div class="mdui-textfield mdui-textfield-floating-label">
+        <label class="mdui-textfield-label">密码</label>
+        <input id="password" type="password" class="mdui-textfield-input" />
+        <center>
+
+          <button class="mdui-btn mdui-btn-raised mdui-ripple" id="btn" onclick="login()">登陆</button>
+
+
+          <?php if (!empty(mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM config where type='xoauth'"))['content'])) { ?>
+            <div style='height:10px'></div>
+            <button class="mdui-btn mdui-btn-raised mdui-ripple" onclick="window.location.href='<?php echo "http://9420.ltd/v1/oauth.php?response_type=code&client_id=" . CLIENT_ID . "&redirect_uri=" . $url . "app/oauth.php" ?>'">第三方登录</button>
+          <?php } ?>
+        </center>
       </div>
     </div>
     <script>
-    var $ = mdui.JQ;
-    function login(){
-      passwd = $('#password').val();
-      $('#btn').attr('disabled',true);
-      $('#btn').text('登陆中...')
-      //构建ajax请求
-      $.ajax({
-        method: 'post',
-        timeout: 10000,
-        url: 'login.php',
-        data:{
-          passwd: passwd
-        },
-        success: function(data)
-        {
-          if(data == '200')
-          {
-            mdui.snackbar({
-              message: '登陆成功,跳转中!',
-              position: 'right-top'
-            });
-            setTimeout("window.location='index.php'",2000)
-          }else{
-            mdui.snackbar({
-              message: '密码错误!',
-              position: 'right-top'
-            });
-            $('#btn').removeAttr('disabled');
-            $('#btn').val('登陆');
+      var $ = mdui.JQ;
+
+      function login() {
+        passwd = $('#password').val();
+        $('#btn').attr('disabled', true);
+        $('#btn').text('登陆中...')
+        //构建ajax请求
+        $.ajax({
+          method: 'post',
+          timeout: 10000,
+          url: 'login.php',
+          data: {
+            passwd: passwd
+          },
+          success: function(data) {
+            if (data == '200') {
+              mdui.snackbar({
+                message: '登陆成功,跳转中!',
+                position: 'right-top'
+              });
+              setTimeout("window.location='index.php'", 2000)
+            } else {
+              mdui.snackbar({
+                message: '密码错误!',
+                position: 'right-top'
+              });
+              $('#btn').removeAttr('disabled');
+              $('#btn').val('登陆');
+            }
+          },
+          complete: function(xhr, textStatus) {
+            $('#btn').text('登陆')
+            if (textStatus == 'timeout') {
+              mdui.snackbar({
+                message: '请求超时!',
+                position: 'right-top'
+              });
+              $('#btn').removeAttr('disabled');
+              $('#btn').val('登陆');
+            }
           }
-        },
-        complete: function (xhr, textStatus) 
-        {
-          $('#btn').text('登陆')
-          if(textStatus == 'timeout')
-          {
-            mdui.snackbar({
-            message: '请求超时!',
-            position: 'right-top'
-          });
-          $('#btn').removeAttr('disabled');
-          $('#btn').val('登陆');
-        }
-      } 
-      });
-    }
+        });
+      }
     </script>
     <?php
     require_once "../footer.php";
--- install.php
+++ install.php
@@ -1,18 +1,21 @@
 <title>短链接安装系统</title>
+
 <body background="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <meta charset="utf-8">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/soxft/cdn@master/mdui/css/mdui.min.css">
-  <script src="https://cdn.jsdelivr.net/gh/soxft/cdn@master/mdui/js/mdui.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/mdui/0.4.3/css/mdui.min.css">
+  <script src="https://cdn.bootcdn.net/ajax/libs/mdui/0.4.3/js/mdui.min.js"></script>
   <br />
-  <center><h2>短链接安装系统</h2></center>
+  <center>
+    <h2>短链接安装系统</h2>
+  </center>
   <?php
   $lockfile = "install.lock";
   if (file_exists($lockfile)) {
     exit("<center><h3>您已经安装过了，如果需要重新安装请先删除根目录下的install.lock(如果你只需要修改内容请访问数据库修改config表<br />如有疑问请加qq群：657529886)</center></h3>");
   }
   if (!isset($_POST['submit'])) {
-    ?>
+  ?>
     <form action="" method="post" enctype="multipart/form-data">
       <div class="mdui-textfield mdui-textfield-floating-label">
         <label class="mdui-textfield-label">数据库地址</label>
@@ -33,12 +36,13 @@
       <br />
       <br />
       <br />
-      <hr><hr>
+      <hr>
+      <hr>
       <br />
       <br />
       <div class="mdui-textfield mdui-textfield-floating-label">
         <label class="mdui-textfield-label">网站域名</label>
-        <input name="url" type="text" class="mdui-textfield-input" value="https://<?php echo$_SERVER['HTTP_HOST'] ?>/" />
+        <input name="url" type="text" class="mdui-textfield-input" value="https://<?php echo $_SERVER['HTTP_HOST'] ?>/" />
       </div>
       <div class="mdui-textfield mdui-textfield-floating-label">
         <label class="mdui-textfield-label">网站标题(网页中所显示的)</label>
@@ -60,24 +64,24 @@
       <center>
         <input class="mdui-btn mdui-btn-raised mdui-ripple" type="submit" name="submit" value="安装" />
       </center>
-      </form>
-      <?php
+    </form>
+  <?php
+  } else {
+    if (empty($_POST['db_host']) || empty($_POST['db_username']) || empty($_POST['db_name']) || empty($_POST['db_password']) || empty($_POST['url']) || empty($_POST['title']) || empty($_POST['title1']) || empty($_POST['pass']) || empty($_POST['passwd'])) {
+      exit("<br/><center><h1>请检查您是否填写完全部内容后重试!</h1></center>");
     } else {
-      if (empty($_POST['db_host']) || empty($_POST['db_username']) || empty($_POST['db_name']) || empty($_POST['db_password']) || empty($_POST['url']) || empty($_POST['title']) || empty($_POST['title1']) || empty($_POST['pass']) || empty($_POST['passwd'])) {
-        exit("<br/><center><h1>请检查您是否填写完全部内容后重试!</h1></center>");
-      } else {
-        $db_host = $_POST['db_host'];
-        $db_username = $_POST['db_username'];
-        $db_password = $_POST['db_password'];
-        $db_name = $_POST['db_name'];
-        $url = $_POST['url'];
-        $title = $_POST['title'];
-        $title1 = $_POST['title1'];
-        $pass = $_POST['pass'];
-        $passwd = $_POST['passwd'];
-      }
-      $conn = mysqli_connect($db_host,$db_username,$db_password,$db_name);
-      if ($conn) {
+      $db_host = $_POST['db_host'];
+      $db_username = $_POST['db_username'];
+      $db_password = $_POST['db_password'];
+      $db_name = $_POST['db_name'];
+      $url = $_POST['url'];
+      $title = $_POST['title'];
+      $title1 = $_POST['title1'];
+      $pass = $_POST['pass'];
+      $passwd = $_POST['passwd'];
+    }
+    $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+    if ($conn) {
       $banx = "CREATE TABLE ban (
         type varchar(10) NOT NULL,
         content	varchar(999) NOT NULL,
@@ -95,61 +99,61 @@
         type mediumtext NOT NULL,
         content mediumtext	NOT NULL
       )";
-      mysqli_query($conn,$banx);
-      mysqli_query($conn,$informationx);
-      mysqli_query($conn,$config);
-      function configAdd($conn,$type,$content)
+      mysqli_query($conn, $banx);
+      mysqli_query($conn, $informationx);
+      mysqli_query($conn, $config);
+      function configAdd($conn, $type, $content)
       {
-        mysqli_query($conn,"INSERT INTO `config` VALUES('$type','$content')");
+        mysqli_query($conn, "INSERT INTO `config` VALUES('$type','$content')");
       }
-      configAdd($conn,'url',$url);
-      configAdd($conn,'title',$title);
-      configAdd($conn,'title1',$title1);
-      configAdd($conn,'pass',$pass);
-      configAdd($conn,'strPolchoice',"123"); //url风格
-      configAdd($conn,'passwd',$passwd);
-      configAdd($conn,'wechat','true');
-      configAdd($conn,'QQ','true');
-      configAdd($conn,'jump','true');
-      configAdd($conn,'urlcheck','true');
-      configAdd($conn,'xoauth','');
-      configAdd($conn,'px','25');
-      } else {
-        exit("<br/><center><h1>数据库连接失败!请确认数据库信息填写正确!</h1></center>");
-      }
-      //写数据库
-      $config_file = "config.php";
-      $config_strings = "<?php\n";
-      $config_strings .= "\$conn=mysqli_connect(\"".$db_host."\",\"".$db_username."\",\"".$db_password."\",\"".$db_name."\");\n";
-      $config_strings .= "\$conns=mysqli_connect(\"".$db_host."\",\"".$db_username."\",\"".$db_password."\",\"information_schema\");\n//你的数据库信息\n";
-      $config_strings .= "function content(\$info)\n";
-      $config_strings .= "{\n";
-      $config_strings .= "global \$conn;    //全局变量\n";
-      $config_strings .= "\$comd = \"SELECT * FROM `config` where `type` = '\$info';\";\n";
-      $config_strings .= "\$sql = mysqli_query(\$conn,\$comd);\n";
-      $config_strings .= "\$arr = mysqli_fetch_assoc(\$sql);\n";
-      $config_strings .= "return \$arr['content'];\n";
-      $config_strings .= "}\n";
-      $config_strings .= "\$url = content(\"url\");         \n//你的网站地址,不要忘记最后的'/'\n";
-      $config_strings .= "\$title1 = content(\"title1\");   \n//网站标题(网页中所显示的)\n";
-      $config_strings .= "\$title = content(\"title\");   \n//网站标题(网页标签所显示的）\n";
-      $config_strings .= "\$pass = content(\"pass\");       \n//短网址后需要的字母或数字个数,推荐4个以上,最长20!(请填写数字)\n";
-      $config_strings .= "\$strPolchoice = content(\"strPolchoice\");   \n//短网址包含的内容,即短网址后会出现的字符\n";
-      $config_strings .= "\$passwd = content(\"passwd\");   \n//设置后台管理密码\n";
-      $config_strings .= "\$px = content(\"px\");      \n//后台短域管理页面一次显示的短域个数\n";
-      $config_strings .= "?>";
-      //文件内容
-      $fp = fopen($config_file,"wb");
-      fwrite($fp,$config_strings);
-      fclose($fp);
-      //写config.php
-      $fp2 = fopen($lockfile,'w');
-      fwrite($fp2,'安装锁文件,请勿删除!');
-      fclose($fp2);
-      //写注册锁
-      echo "<br/><center><h1>安装成功!4s后将为您自动跳转到后台登录界面!</h1></center>";
-      echo "<br/><center><h1>你可以进入后台进入更多设置！</h1></center>";
-      echo "<br/><center><h2>非宝塔一键部署用户请注意,你还需要自己手动配置网站伪静态.网站伪静态配置信息请参考根目录下`README.md`中所写内容.</h2></center>";
-      header("Refresh:4;url=\"./admin/\"");
+      configAdd($conn, 'url', $url);
+      configAdd($conn, 'title', $title);
+      configAdd($conn, 'title1', $title1);
+      configAdd($conn, 'pass', $pass);
+      configAdd($conn, 'strPolchoice', "123"); //url风格
+      configAdd($conn, 'passwd', $passwd);
+      configAdd($conn, 'wechat', 'true');
+      configAdd($conn, 'QQ', 'true');
+      configAdd($conn, 'jump', 'true');
+      configAdd($conn, 'urlcheck', 'true');
+      configAdd($conn, 'xoauth', '');
+      configAdd($conn, 'px', '25');
+    } else {
+      exit("<br/><center><h1>数据库连接失败!请确认数据库信息填写正确!</h1></center>");
     }
-    ?>
+    //写数据库
+    $config_file = "config.php";
+    $config_strings = "<?php\n";
+    $config_strings .= "\$conn=mysqli_connect(\"" . $db_host . "\",\"" . $db_username . "\",\"" . $db_password . "\",\"" . $db_name . "\");\n";
+    $config_strings .= "\$conns=mysqli_connect(\"" . $db_host . "\",\"" . $db_username . "\",\"" . $db_password . "\",\"information_schema\");\n//你的数据库信息\n";
+    $config_strings .= "function content(\$info)\n";
+    $config_strings .= "{\n";
+    $config_strings .= "global \$conn;    //全局变量\n";
+    $config_strings .= "\$comd = \"SELECT * FROM `config` where `type` = '\$info';\";\n";
+    $config_strings .= "\$sql = mysqli_query(\$conn,\$comd);\n";
+    $config_strings .= "\$arr = mysqli_fetch_assoc(\$sql);\n";
+    $config_strings .= "return \$arr['content'];\n";
+    $config_strings .= "}\n";
+    $config_strings .= "\$url = content(\"url\");         \n//你的网站地址,不要忘记最后的'/'\n";
+    $config_strings .= "\$title1 = content(\"title1\");   \n//网站标题(网页中所显示的)\n";
+    $config_strings .= "\$title = content(\"title\");   \n//网站标题(网页标签所显示的）\n";
+    $config_strings .= "\$pass = content(\"pass\");       \n//短网址后需要的字母或数字个数,推荐4个以上,最长20!(请填写数字)\n";
+    $config_strings .= "\$strPolchoice = content(\"strPolchoice\");   \n//短网址包含的内容,即短网址后会出现的字符\n";
+    $config_strings .= "\$passwd = content(\"passwd\");   \n//设置后台管理密码\n";
+    $config_strings .= "\$px = content(\"px\");      \n//后台短域管理页面一次显示的短域个数\n";
+    $config_strings .= "?>";
+    //文件内容
+    $fp = fopen($config_file, "wb");
+    fwrite($fp, $config_strings);
+    fclose($fp);
+    //写config.php
+    $fp2 = fopen($lockfile, 'w');
+    fwrite($fp2, '安装锁文件,请勿删除!');
+    fclose($fp2);
+    //写注册锁
+    echo "<br/><center><h1>安装成功!4s后将为您自动跳转到后台登录界面!</h1></center>";
+    echo "<br/><center><h1>你可以进入后台进入更多设置！</h1></center>";
+    echo "<br/><center><h2>非宝塔一键部署用户请注意,你还需要自己手动配置网站伪静态.网站伪静态配置信息请参考根目录下`README.md`中所写内容.</h2></center>";
+    header("Refresh:4;url=\"./admin/\"");
+  }
+  ?>
