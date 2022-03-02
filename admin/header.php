d0336099fb0d425434f9ef38ab0cc440ccf80617
xcsoft <email@xcsoft.top>
2022-03-02 19:49:16 +0800

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -19,8 +19,8 @@ $password = $_SESSION['password'];
   <link rel="icon" type="image/x-icon" href="https://cdn.jsdelivr.net/gh/soxft/cdn@1.0.0/urlshorting/favicon.ico" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <meta charset="utf-8">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/soxft/cdn@1.0.0/mdui/css/mdui.min.css">
-  <script src="https://cdn.jsdelivr.net/gh/soxft/cdn@1.0.0/mdui/js/mdui.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/mdui@0.4.3/dist/js/mdui.min.js"></script>
   <style>
     a {
       text-decoration:none
